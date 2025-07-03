c5121553d4493b3582bc13ea573a5054cdc98418
ayakovenko <ayakovenko@scalea.tech>
2025-07-03 10:56:02 +0300

    save localchanges befor pull

--- Converter/src/app/ConverterFtoC.java
+++ Converter/src/app/ConverterFtoC.java
@@ -3,6 +3,9 @@ package app;
 public class ConverterFtoC {
 
     public static void main(String[] args) {
+
+        System.out.println("Application for converting degrees from celsius to fahrenheit and back");
+
         int fahrenheit = 100;
 
         int degreesCelConverted = convertFtoC(fahrenheit);
