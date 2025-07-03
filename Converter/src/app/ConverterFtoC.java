0274b222e9920e1207b0a85f01cc0c0a097aa5ba
ayakovenko <ayakovenko@scalea.tech>
2025-07-03 10:58:12 +0300

    add functionality of converting celsius to fahrenheit

--- Converter/src/app/ConverterFtoC.java
+++ Converter/src/app/ConverterFtoC.java
@@ -6,15 +6,22 @@ public class ConverterFtoC {
 
         System.out.println("Application for converting degrees from celsius to fahrenheit and back");
 
+        int celsius = 30;
         int fahrenheit = 100;
 
+        int degreesFarConverted = convertCtoF(celsius);
         int degreesCelConverted = convertFtoC(fahrenheit);
 
+        System.out.println(celsius + "℃ is " + degreesFarConverted + "°F");
         System.out.println(fahrenheit + "°F is " + degreesCelConverted + "℃");
     }
 
 
     public static int convertFtoC(int degrees) {
         return (degrees - 32) * 5 / 9;
+    }
+
+    public static int convertCtoF(int degrees) {
+        return (degrees * 9 / 5) + 32;
     }
 }
