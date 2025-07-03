25b04cfa0a7fae196ac83a48a7eecdb8202f22cd
ayakovenko <ayakovenko@scalea.tech>
2025-07-03 10:44:11 +0300

    initial commit for class ConverterFtoC

---
+++ Converter/src/app/ConverterFtoC.java
@@ -0,0 +1,9 @@
+package app;
+
+public class ConverterFtoC {
+
+    public static void main(String[] args) {
+        System.out.println("Application for converting degrees from celsius to fahrenheit and back");
+    }
+
+}
